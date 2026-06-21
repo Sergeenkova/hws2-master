3b9db13d70ba40d0925410bd66b0e50323430bd4
Olga Sergeenkova <o.y.sergeenkova@gmail.com>
2026-06-22 00:22:36 +0200

    refactor: add correct code and styles HW8 (sprint3)

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,20 +1,28 @@
-import {UserType} from '../HW8'
+import { UserType } from "../HW8";
 
 type ActionType =
-    | { type: 'sort'; payload: 'up' | 'down' }
-    | { type: 'check'; payload: number }
+  | { type: "sort"; payload: "up" | "down" }
+  | { type: "check"; payload: number };
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
-    switch (action.type) {
-        case 'sort': { // by name
-
-            return state // need to fix
-        }
-        case 'check': {
-
-            return state // need to fix
-        }
-        default:
-            return state
+export const homeWorkReducer = (
+  state: UserType[],
+  action: ActionType,
+): UserType[] => {
+  // need to fix any
+  switch (action.type) {
+    case "sort": {
+      // by name
+      if (action.payload === "up") {
+        return [...state].sort((a, b) => a.name.localeCompare(b.name));
+      } else if (action.payload === "down") {
+        return [...state].sort((a, b) => b.name.localeCompare(a.name));
+      }
+      return state;
     }
-}
+    case "check": {
+      return state.filter((u) => u.age >= action.payload);
+    }
+    default:
+      return state;
+  }
+};
