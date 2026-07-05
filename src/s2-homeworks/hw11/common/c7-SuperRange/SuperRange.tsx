6eb15971856451de07bea206a0108479298d60d7
Olga Sergeenkova <o.y.sergeenkova@gmail.com>
2026-07-05 16:51:55 +0200

    fix: add correct styles for Slider HW10 (sprint 4)

--- src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
+++ src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
@@ -1,15 +1,36 @@
-import React from 'react'
-import {Slider, SliderProps} from '@mui/material'
+import { Slider, SliderProps } from "@mui/material";
+import React from "react";
 
 const SuperRange: React.FC<SliderProps> = (props) => {
-    return (
-        <Slider
-            sx={{ // стили для слайдера // пишет студент
-                
-            }}
-            {...props} // отдаём слайдеру пропсы если они есть (value например там внутри)
-        />
-    )
-}
+  return (
+    <Slider
+      sx={{
+        color: "#1976d2",
+        height: 8,
+        "& .MuiSlider-thumb": {
+          width: 18,
+          height: 18,
+          backgroundColor: "#fff",
+          border: "2px solid currentColor",
+          boxShadow: "0 2px 6px rgba(0,0,0,0.12)",
+        },
+        "& .MuiSlider-track": {
+          height: 8,
+          borderRadius: 8,
+        },
+        "& .MuiSlider-rail": {
+          height: 8,
+          borderRadius: 8,
+          opacity: 0.38,
+        },
+        "& .MuiSlider-valueLabel": {
+          backgroundColor: "#1976d2",
+          color: "#fff",
+        },
+      }}
+      {...props} // отдаём слайдеру пропсы если они есть (value например там внутри)
+    />
+  );
+};
 
-export default SuperRange
+export default SuperRange;
