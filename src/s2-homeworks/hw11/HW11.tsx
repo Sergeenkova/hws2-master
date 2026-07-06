c862502ad9a12d189d31049ce13f6ec645b78e9a
Olga Sergeenkova <o.y.sergeenkova@gmail.com>
2026-07-06 11:30:01 +0200

    fix: add correct styles for Slider (v2) HW11 (sprint 4)

--- src/s2-homeworks/hw11/HW11.tsx
+++ src/s2-homeworks/hw11/HW11.tsx
@@ -29,30 +29,26 @@ function HW11() {
   return (
     <div id={"hw11"}>
       <div className={s2.hwTitle}>Homework #11</div>
-
       <div className={s2.hw}>
         <div className={s.container}>
           <div className={s.wrapper}>
             <span id={"hw11-value"} className={s.number}>
               {value1}
             </span>
-            <SuperRange
-              id={"hw11-single-slider"}
-              // сделать так чтоб value1 изменялось // пишет студент
-              value={value1}
-              onChange={change}
-            />
+              <SuperRange
+                id={"hw11-single-slider"}
+                value={value1}
+                onChange={change}
+              />
           </div>
           <div className={s.wrapper}>
             <span id={"hw11-value-1"} className={s.number}>
               {value1}
             </span>
-            <SuperRange
-              id={"hw11-double-slider"}
-              // сделать так чтоб value1/2 изменялось // пишет студент
-              value={[value1, value2]}
-              onChange={change}
-            />
+              <SuperRange
+                value={[value1, value2]}
+                onChange={change}
+              />
             <span id={"hw11-value-2"} className={s.number}>
               {value2}
             </span>
--- src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
+++ src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
@@ -5,28 +5,7 @@ const SuperRange: React.FC<SliderProps> = (props) => {
   return (
     <Slider
       sx={{
-        color: "#1976d2",
-        height: 8,
-        "& .MuiSlider-thumb": {
-          width: 18,
-          height: 18,
-          backgroundColor: "#fff",
-          border: "2px solid currentColor",
-          boxShadow: "0 2px 6px rgba(0,0,0,0.12)",
-        },
-        "& .MuiSlider-track": {
-          height: 8,
-          borderRadius: 8,
-        },
-        "& .MuiSlider-rail": {
-          height: 8,
-          borderRadius: 8,
-          opacity: 0.38,
-        },
-        "& .MuiSlider-valueLabel": {
-          backgroundColor: "#1976d2",
-          color: "#fff",
-        },
+  
       }}
       {...props} // отдаём слайдеру пропсы если они есть (value например там внутри)
     />
