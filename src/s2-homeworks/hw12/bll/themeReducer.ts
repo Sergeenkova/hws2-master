4fa16f97224eaeba7705b1bc04d057716af2352a
Olga Sergeenkova <o.y.sergeenkova@gmail.com>
2026-07-17 20:46:34 +0200

    refactor: add correct code and styles HW12 (sprint 4)

--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -1,14 +1,22 @@
 const initState = {
-    themeId: 1,
+  themeId: 1,
+};
+
+type ThemeAction = {
+  type: "SET_THEME_ID";
+  id: number; 
 }
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
-    switch (action.type) {
-        // дописать
-
-        default:
-            return state
+export const themeReducer = (state = initState, action: ThemeAction): any => {
+  // fix any
+  switch (action.type) {
+    // дописать
+    case "SET_THEME_ID": {
+      return { ...state, themeId: action.id };
     }
-}
+    default:
+      return state;
+  }
+};
 
-export const changeThemeId = (id: number): any => ({ type: 'SET_THEME_ID', id }) // fix any
+export const changeThemeId = (id: number): ThemeAction => ({type: "SET_THEME_ID", id}); // fix any
